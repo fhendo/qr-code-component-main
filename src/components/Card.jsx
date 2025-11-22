d19087e62a34840ffbb7914caffd8f31ace4777b
Fábio Endo <fendo@bsgi.org.br>
2025-11-22 14:27:47 -0300

    Ajustes finos no layout

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,16 +2,16 @@ import { useState } from 'react'
 
 const Card = () => {
   return (
-    <div className='max-w-xs bg-white rounded-3xl mt-2 p-4 flex flex-col justify-content items-center shadow-lg'>
+    <div className='max-w-xs bg-white rounded-3xl mt-3 p-4 flex flex-col justify-content items-center shadow-lg'>
       <div>
         <img src='./assets/image-qr-code.png' className='rounded-xl' />
       </div>
 
-      <div className='px-2 py-5'>
+      <div className='px-2 pt-5'>
         <h1 className='text-2xl font-display text-slate-900 font-bold text-center'>Improve your front-end skills by building projects</h1>
       </div>
 
-      <div className='px-3 mb-5'>
+      <div className='px-4 mb-5 pt-3'>
         <p className='text-center text-[15px] font-display text-slate-500'>Scan the QR code to visit Frontend Mentor and take yout coding skills to the next level</p>
       </div>
     </div>
